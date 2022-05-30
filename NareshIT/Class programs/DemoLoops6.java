bd36697ca5402ce353807c50837d2873d93d9710
Shirish <shirish.baisane@gmail.com>
2022-05-30 17:16:37 +0530

    DemoLoops6 exercise completed

--- NareshIT/Class programs/DemoLoops6.java
+++ NareshIT/Class programs/DemoLoops6.java
@@ -1,3 +1,21 @@
 /*
 WAP to read a number and display the reverse the given number.
 */
+
+import java.util.Scanner;
+
+public class DemoLoops6{
+	public static void main(String[]args){
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num = s.nextInt();
+		System.out.println("Reverse of the number is:");
+		while(num!=0){
+			int x = num%10;
+			System.out.print(x);
+			num/=10;
+		}//end of loop
+		
+		s.close();
+	}
+}

7b7f52421af30d5a6ac5d84ec4072ce167ebdbb1
Shirish <shirish.baisane@gmail.com>
2022-05-30 18:11:47 +0530

    DemoLoops8Assgn assignment completed

--- NareshIT/Class programs/DemoLoops8Assgn.java
+++ NareshIT/Class programs/DemoLoops8Assgn.java
@@ -4,3 +4,32 @@ o/p:
 ====
 11 21 31 41 51 ............ 
 */
+
+import java.util.Scanner;
+
+class Reverse{
+	int rev(int x){
+		int reverse=0;
+		while(x!=0){
+			int r=x%10;
+			reverse = (reverse*10)+r;
+			x/=10;
+		}//end of loop
+		return reverse;
+	}//end of method
+}
+
+public class DemoLoops8Assgn{
+	public static void main(String[]args){
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a number upto which you want to reverse the number: ");
+		int num = s.nextInt();
+		System.out.println("");
+		Reverse ob = new Reverse();
+		for(int i=11; i<=num; i++){
+			System.out.print(ob.rev(i)+" ");//method_call
+		}//end of loop
+		
+		s.close();
+	}
+}

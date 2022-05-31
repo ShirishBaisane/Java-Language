2d899df071b417d32cfac2d8d6e9969a1636335a
Shirish <shirish.baisane@gmail.com>
2022-05-31 19:30:20 +0530

    DemoCon1 concept program completed

--- NareshIT/Class programs/DemoCon1.java
+++ NareshIT/Class programs/DemoCon1.java
@@ -1,3 +1,23 @@
 /*
 wAP to understand the working of Constructor_without_parameter.
 */
+
+class CTest1{
+	int a=10;//Instance_variable
+	CTest1(){
+		System.out.println("===CTest1() Constructor===");
+		System.out.println("a: "+a);
+	}//Constructor
+	void dis(){
+		System.out.println("***dis() Method***");
+		System.out.println("a: "+a);
+	}//Instance_method
+}//SubClass
+
+class DemoCon1{
+	public static void main(String[]args){
+		CTest1 ob = new CTest1();//Constructor_call
+		ob.dis();//Instance_method_call
+		ob.dis();//Instance_method_call
+	}
+}

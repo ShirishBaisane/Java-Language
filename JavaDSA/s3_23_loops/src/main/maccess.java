1951342e26f9b84bde9ff618be3d9c214fb50b09
Shirish <shirish.baisane@gmail.com>
2022-10-11 23:32:55 +0530

    section 3, Q.23 completed

--- JavaDSA/s3_23_loops/src/main/maccess.java
+++ JavaDSA/s3_23_loops/src/main/maccess.java
@@ -1,10 +1,33 @@
 package main;
 
+import java.util.Scanner;
+
 public class maccess {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);//Scanner_object created
+		System.out.println("Enter the number:");
+		int num = scan.nextInt();
+		maccess m = new maccess();
+		System.out.println("");
+		m.factors(num);//object_method_call
+	
+		scan.close();
 
+	}
+	
+	void factors(int num) {
+		while(num!=1)
+		{
+			for(int i=2; i<=num; i++)
+			{
+				while(num%i==0)
+				{
+					System.out.print(i+"\t");
+					num = num/i;
+				}//end of inner while
+			}//end of for
+		}//end of outer while
 	}
 
 }

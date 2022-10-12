91e11e2fdb616f27897c5a80d9820bb81341b6cb
Shirish <shirish.baisane@gmail.com>
2022-10-12 17:55:24 +0530

    section 3, Intermediate Q.2 Electricity Bill using loops and OOP concepts program is in progress with only loops implication remaining

--- JavaDSA/s3_i2_loops_oop/src/BillDetais/Details.java
+++ JavaDSA/s3_i2_loops_oop/src/BillDetais/Details.java
@@ -1,11 +1,11 @@
 package BillDetais;
 
 public class Details {
-	private double charge = 10.078125;
+	double charge = 10.078125;
 	 
-	int bN, bU, cR, pR, units;
-	
-	Details(int bNumber, int bUnit, int cReading, int pReading)
+	int bN, bU, cR, pR;
+	//Constructor with parameter
+	public Details(int bNumber, int bUnit, int cReading, int pReading)
 	{
 		bN = bNumber;
 		bU = bUnit;
@@ -13,5 +13,36 @@ public class Details {
 		pR = pReading;
 	}
 	
+	//getter_method for Total bill amount
+	public void getTBL() {
+		System.out.println("Bill Number: "+bN);
+		System.out.println("Billing Unit: "+bU);
+		System.out.println("Tariff/Category: Residential");
+		System.out.println("Current Reading: "+cR);
+		System.out.println("Previous Reading: "+pR);
+		System.out.println("Charge per unit(Rs.): "+charge);
+		System.out.println("Total units: "+(cR-pR));
+		System.out.println("Bill Amount(Rs.) (till dueDate): "+calculate(cR, pR, charge));//static_method_call
+		System.out.println("");
+	}
+	
+	//getter_method for Total bill amount
+	public void getABL() {
+		System.out.println("Bill Number: "+bN);
+		System.out.println("Billing Unit: "+bU);
+		System.out.println("Tariff/Category: Residential");
+		System.out.println("Current Reading: "+cR);
+		System.out.println("Previous Reading: "+pR);
+		System.out.println("Charge per unit(Rs.): "+charge);
+		System.out.println("Total units: "+(cR-pR));
+		System.out.println("Bill Amount(Rs.) (till dueDate): "+calculate(cR, pR, charge));//static_method_call
+		System.out.println("Bill Amount(Rs.) (after dueDate): "+(calculate(cR, pR, charge)+40));//static_method_call
+	}
+	
+	static double calculate(int cR, int pR, double c) {
+		int units = cR - pR;
+		double tba = (Double)(units*c);
+		return tba;
+	}//end of method
 
 }
--- JavaDSA/s3_i2_loops_oop/src/main/maccess.java
+++ JavaDSA/s3_i2_loops_oop/src/main/maccess.java
@@ -1,9 +1,32 @@
 package main;
 
+import java.util.Scanner;
+
+import BillDetais.Details;
+
 public class maccess {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int bN, bU, cR, pR;
+		System.out.println("Enter Bill Number: ");
+		bN = scan.nextInt();
+		System.out.println("Enter Billing Unit: ");
+		bU = scan.nextInt();
+		System.out.println("Enter Current Reading: ");
+		cR = scan.nextInt();
+		System.out.println("Enter Previous Reading: ");
+		pR = scan.nextInt();
+		
+		//Creating object using Constructor with parameter
+		Details det = new Details(bN, bU, cR, pR);
+		
+		det.getTBL();//getter_method_call
+		det.getABL();//getter_method_call
+		
+		System.out.println("");
+		
+		scan.close();
 
 	}
 

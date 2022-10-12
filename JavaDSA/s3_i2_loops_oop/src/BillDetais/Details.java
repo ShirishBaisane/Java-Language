4fb94d503cd407b4c09721a1b95fdfb8301a0c8e
Shirish <shirish.baisane@gmail.com>
2022-10-12 17:05:54 +0530

    section 3, Intermediate Q.2 Electricity Bill using loops and OOP concepts program started

---
+++ JavaDSA/s3_i2_loops_oop/src/BillDetais/Details.java
@@ -0,0 +1,17 @@
+package BillDetais;
+
+public class Details {
+	private double charge = 10.078125;
+	 
+	int bN, bU, cR, pR, units;
+	
+	Details(int bNumber, int bUnit, int cReading, int pReading)
+	{
+		bN = bNumber;
+		bU = bUnit;
+		cR = cReading; 
+		pR = pReading;
+	}
+	
+
+}

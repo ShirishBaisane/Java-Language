0e7ce31d7ee4efc78e21bc49c282ed9577b3642c
Shirish <shirish.baisane@gmail.com>
2022-10-13 22:53:16 +0530

    section 3, Intermediate Q11 completed

--- JavaDSA/s3_i5_to_i26/src/i11/maccess.java
+++ JavaDSA/s3_i5_to_i26/src/i11/maccess.java
@@ -1,9 +1,30 @@
 package i11;
 
+import java.util.Scanner;
+
 public class maccess {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("==Find the Compound Interest==");
+		System.out.println("Enter the initial Principal balance: ");
+		double prin = scan.nextDouble();
+		System.out.println("Enter the Rate: ");
+		double rate = scan.nextDouble();
+		System.out.println("Enter the number of times interest applied per year: ");
+		int n = scan.nextInt();
+		System.out.println("Enter the Duartion(years): ");
+		int t = scan.nextInt();
+		double amt=prin;
+		int a = n*t;
+		double b = rate/(100*n);
+		for(int i=1; i<=a; i++) {
+			amt = amt*(1+b);
+		}
+		System.out.println("C.I.: "+(amt-prin));
+		
+		scan.close();
 	}
 
 }

49d2a4d28d9f842997b1c745743829250d3805c8
Shirish <shirish.baisane@gmail.com>
2022-10-13 23:33:01 +0530

    section 3, Intermediate Q17(Palindrome) completed

--- JavaDSA/s3_i5_to_i26/src/i17/maccess.java
+++ JavaDSA/s3_i5_to_i26/src/i17/maccess.java
@@ -1,9 +1,30 @@
 package i17;
 
+import java.util.Scanner;
+
 public class maccess {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num1, num2=0;
+		System.out.println("==Check if the number is Palindrome==");
+		System.out.println("Enter the number: ");
+		num1 = scan.nextInt();
 		
+		int n = num1;
+		while(n!=0) {
+			int a = n%10;
+			num2 = (num2*10)+a;
+			n = n/10;
+		}
+		
+		if(num1==num2) {
+			System.out.println("Number is palindrome");
+		}
+		else {
+			System.out.println("Number is not palindrome");
+		}
+		scan.close();
 	}
 
 }

9223638f872eec021a4970153258ad94b9d929e0
Shirish <shirish.baisane@gmail.com>
2022-10-14 23:52:17 +0530

    section 4, Funtions, Q7(prime no funtion) completed

--- JavaDSA/s4_Funtions/src/q7/maccess.java
+++ JavaDSA/s4_Funtions/src/q7/maccess.java
@@ -1,10 +1,40 @@
 package q7;
 
+import java.util.Scanner;
+
 public class maccess {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("===Find if the numbers is Prime or not===");
+		System.out.println("Num: ");
+		int num = scan.nextInt();
+		
+		boolean p = isPrime(num);
+		if(p==true) {
+			System.out.println("The number "+num+" is a Prime number");
+		}
+		else {
+			System.out.println("The number "+num+" is not a Prime number");
+		}
+		scan.close();
 
 	}
+	//Function to check if the number is Prime or not
+	static boolean isPrime(int n) {
+		boolean p = false;
+		int count =0;
+		for(int i=1; i<=n; i++) {
+			if(n%i==0) {
+				count++;
+			}
+		}
+		if(count==2) {
+			p = true;
+		}
+		return p;
+	}
+	
 
 }

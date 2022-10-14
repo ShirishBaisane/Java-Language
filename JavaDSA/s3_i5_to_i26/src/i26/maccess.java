8242944c9fbf50759bc21629a392bdcc736b7aa6
Shirish <shirish.baisane@gmail.com>
2022-10-14 20:05:33 +0530

    section 3, Intermediate Q26 Completed

--- JavaDSA/s3_i5_to_i26/src/i26/maccess.java
+++ JavaDSA/s3_i5_to_i26/src/i26/maccess.java
@@ -1,9 +1,32 @@
 package i26;
 
+import java.util.Scanner;
+
 public class maccess {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num=1, sumN=0, sumE=0, sumO=0;
+		System.out.println("==Sum of Negative numbers, Positive Even numbers, Positive Odd numbers==");
+		while(num!=0) {
+			System.out.println("Enter the number OR press '0' to quit: ");
+			num = scan.nextInt();
+			if(num<0) {
+				sumN = sumN+num;
+			}
+			else if(num>0 && num%2==0) {
+				sumE = sumE+num;
+			}
+			else  {
+				sumO = sumO+num;
+			}
+		}
 		
+		System.out.println("Sum of Negative numbers is: "+sumN);
+		System.out.println("Sum of Positive Even numbers is: "+sumE);
+		System.out.println("Sum of Positive Odd numbers is: "+sumO);
+		
+		scan.close();
 
 	}
 

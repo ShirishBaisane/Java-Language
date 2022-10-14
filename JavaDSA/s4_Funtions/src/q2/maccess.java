d07de3d797fd40144836fbae2bb86a2f541032c6
Shirish <shirish.baisane@gmail.com>
2022-10-14 21:55:46 +0530

    section 4, Funtions, Q2 Completed

--- JavaDSA/s4_Funtions/src/q2/maccess.java
+++ JavaDSA/s4_Funtions/src/q2/maccess.java
@@ -1,9 +1,27 @@
 package q2;
 
+import java.util.Scanner;
+
 public class maccess {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("===Find the number is Even or Odd===");
+		System.out.println("Num: ");
+		int num = scan.nextInt();
+		
+		maccess m = new maccess();
+		m.check(num);
+	}
+	
+	void check(int num) {
+		if(num%2==0) {
+			System.out.println("Even number.");
+		}
+		else {
+			System.out.println("Odd number.");
+		}
 	}
 
 }

4dfb74a2392be2759908592bb76cb30af0d00ac5
Shirish <shirish.baisane@gmail.com>
2022-10-14 22:09:50 +0530

    section 4, Funtions, Q3 completed

--- JavaDSA/s4_Funtions/src/q3/maccess.java
+++ JavaDSA/s4_Funtions/src/q3/maccess.java
@@ -1,10 +1,33 @@
 package q3;
 
+import java.util.Scanner;
+
 public class maccess {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("===Find out if a person is eligible to vote===");
+		System.out.println("Enter the age of the person: ");
+		int age = scan.nextInt();
 		
+		boolean p = check(age);
+		if(p==true) {
+			System.out.println("The person is eligible for voting.");
+		}
+		else {
+			System.out.println("The person is not eligible for voting.");
+		}
+		scan.close();
 
+	}
+	//function to verify age
+	static boolean check(int age) {
+		if(age>=18) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 }

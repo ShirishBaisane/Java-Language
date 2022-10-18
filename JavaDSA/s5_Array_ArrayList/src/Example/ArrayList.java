981723f30e870ce315621f39f13b389b53445021
Shirish <shirish.baisane@gmail.com>
2022-10-19 00:39:09 +0530

    section 5, ArrayList examples started

---
+++ JavaDSA/s5_Array_ArrayList/src/Example/ArrayList.java
@@ -0,0 +1,17 @@
+package Example;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		//Syntax for ArrayList
+		//ArrayList<Integer> list = new ArrayList<>(5);
+		
+		scan.close();
+
+	}
+
+}

f544680c8704886c04b0d3c1288d28cbfc7fb1ce
Shirish <shirish.baisane@gmail.com>
2022-11-16 00:51:39 +0530

    7. Search in rotated sorted array using pivot, done.

---
+++ JavaDSA/s6_searching/src/BinarySearchQuestions/SearchInRotatedSA.java
@@ -0,0 +1,64 @@
+package BinarySearchQuestions;
+
+public class SearchInRotatedSA {
+
+	public static void main(String[] args) {
+		int[] arr = {4,5,6,3,2,1,0};
+		int target = 5;
+		System.out.println("Pivot element Index is: "+findPivot(arr));
+		System.out.println("Index of target element "+target+" is: "+searchInRSA(arr, target));
+
+	}
+	
+	static int binarySearch(int[] arr, int target, int start, int end) {
+			while(start<=end) {
+			int mid = start + (end-start)/2;
+			if(target<arr[mid]) {
+				end = mid-1;
+			}
+			else if(target>arr[mid]) {
+				start = mid+1;
+			}
+			else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+	
+	static int findPivot(int[] arr) {
+		int start = 0, end = arr.length-1;
+		while(start<=end) {
+			int mid = start + (end-start)/2;
+			if(arr[mid]>arr[mid+1] && mid<end) {
+				return mid;
+			}
+			if(arr[mid]<arr[mid-1] && mid>start) {
+				return mid-1;
+			}
+			if(arr[start]>=arr[mid]) {
+				end = mid-1;
+			} else {
+				start = mid+1;
+			}
+		}
+		return -1;
+	}
+	
+	static int searchInRSA(int[] arr, int target) {
+		int pivot = findPivot(arr);
+		if(pivot==-1) {
+			return binarySearch(arr, target, 0, arr.length-1);
+		}
+		if(arr[pivot]==target) {
+			return pivot;
+		}
+		if(target>=arr[0]) {
+			return binarySearch(arr, target, 0, pivot-1);
+		}
+		return binarySearch(arr, target, pivot+1, arr.length-1);
+	}
+	
+	
+
+}

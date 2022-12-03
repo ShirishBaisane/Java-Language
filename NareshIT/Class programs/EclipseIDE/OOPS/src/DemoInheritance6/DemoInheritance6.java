65dde7b50862ab7edb10963ebf2611850e42137c
Shirish <shirish.baisane@gmail.com>
2022-12-03 17:21:08 +0530

    OOPS concepts: Inheritance programs6(method over-riding) done.

---
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DemoInheritance6/DemoInheritance6.java
@@ -0,0 +1,8 @@
+package DemoInheritance6;
+
+public class DemoInheritance6 {
+	public static void main(String [] args) {
+		CClass ob = new CClass();
+		ob.calculate(123);
+	}
+}
---
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DemoInheritance7/CClass.java
@@ -0,0 +1,5 @@
+package DemoInheritance7;
+
+public class CClass {
+
+}

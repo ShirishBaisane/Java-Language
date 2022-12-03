ddf4560d66875f327d7b1f53bc981485316f8916
Shirish <shirish.baisane@gmail.com>
2022-12-03 17:24:22 +0530

    OOPS concepts: Inheritance programs7(method hiding) done.

--- NareshIT/Class programs/EclipseIDE/OOPS/src/DemoInheritance7/CClass.java
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DemoInheritance7/CClass.java
@@ -1,5 +1,9 @@
 package DemoInheritance7;
 
-public class CClass {
+public class CClass extends PClass {
+	public static void m(int x) {
+		System.out.println("===CClass m(x)===");
+		System.out.println("The value x: "+x);
+	}
 
 }
---
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DemoInheritance7/DemoInheritance7.java
@@ -0,0 +1,10 @@
+package DemoInheritance7;
+
+public class DemoInheritance7 {
+
+	public static void main(String[] args) {
+		CClass.m(123);//Method_call
+
+	}
+
+}

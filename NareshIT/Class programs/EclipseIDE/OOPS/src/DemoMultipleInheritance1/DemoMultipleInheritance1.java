8daa5635d147cff2b11f6d897e69a463d99328a4
Shirish <shirish.baisane@gmail.com>
2022-12-22 12:05:14 +0530

    OOPS concepts: Multiple Inheritance model using Interfaces model-1(Class implementing more than one Interface) done.

---
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DemoMultipleInheritance1/DemoMultipleInheritance1.java
@@ -0,0 +1,14 @@
+package DemoMultipleInheritance1;
+
+public class DemoMultipleInheritance1 {
+
+	public static void main(String[] args) {
+		IClass ob = new IClass();
+		ob.m1(12);
+		ob.m2(13);
+		ob.m3(14);
+		ob.dis();
+
+	}
+
+}
---
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DemoMultipleInheritance1/ITest1.java
@@ -0,0 +1,8 @@
+package DemoMultipleInheritance1;
+
+public interface ITest1 {
+	public static final int a = 10;
+	public abstract void m1(int x);
+	public abstract void m2(int y);
+
+}

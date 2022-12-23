db4f84e025292eb13aa5ec6347e12c3f81213a84
Shirish <shirish.baisane@gmail.com>
2022-12-23 19:56:53 +0530

    OOPS concepts: InnerClass -> Instance Member InnerClass program done.

---
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DemoInnerClass2/SubClass11.java
@@ -0,0 +1,27 @@
+package DemoInnerClass2;
+
+public class SubClass11 {
+	public int a=10;
+	public static int b=20;
+	
+	public void m1() {
+		System.out.println("===OuterClass method m1()===");
+		System.out.println("The value a: "+1);
+		System.out.println("The value b "+b);
+	}
+	
+	public class SubClass22{
+		public void m2() {
+			System.out.println("===InnerClass Instance method m2()===");
+			System.out.println("The value a: "+a);
+			System.out.println("The value b: "+b);
+		}
+		
+		public static void m22() {
+			System.out.println("===Inner =Class static method m22()===");
+			//System.out.println("The value a: "+a);//Error
+			System.out.println("The value b: "+b);
+		}
+	}//Instance_member_InnerClass
+
+}//OuterClass

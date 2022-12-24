212db1b6f44521798657a2015be12dfc870f2bc8
Shirish <shirish.baisane@gmail.com>
2022-12-24 19:13:31 +0530

    OOPS concepts: DesignConcept-2 program done.

---
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DesignApp2/DemoDesign2.java
@@ -0,0 +1,11 @@
+package DesignApp2;
+
+public class DemoDesign2 {
+
+	public static void main(String[] args) {
+		ITest ob = Access.getRef();
+		ob.m(123);
+
+	}
+
+}
---
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DesignApp2/ITest.java
@@ -0,0 +1,6 @@
+package DesignApp2;
+
+public interface ITest {
+	public abstract void m(int x);
+
+}

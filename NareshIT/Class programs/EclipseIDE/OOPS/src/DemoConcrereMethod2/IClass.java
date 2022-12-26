ec12b3a87079302ebb93635721db7ba048a24070
Shirish <shirish.baisane@gmail.com>
2022-12-26 11:28:13 +0530

    OOPS concepts: Concrete Methods in Interfaces(default concrete method) program done.

---
+++ NareshIT/Class programs/EclipseIDE/OOPS/src/DemoConcrereMethod2/IClass.java
@@ -0,0 +1,9 @@
+package DemoConcrereMethod2;
+
+public class IClass implements ITest {
+	public void m1(int x) {
+		System.out.println("===Implemented method m1(x)===");
+		System.out.println("x: "+x);
+	}
+
+}

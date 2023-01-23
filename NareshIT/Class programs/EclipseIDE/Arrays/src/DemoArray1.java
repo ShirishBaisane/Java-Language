4f99c8cd65098def95cf8667046a4a6ba62c6cdc
Shirish <shirish.baisane@gmail.com>
2023-01-23 19:56:44 +0530

    Arrays: DemoArray1 completed with Entended-for & Spliterator loops.

---
+++ NareshIT/Class programs/EclipseIDE/Arrays/src/DemoArray1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+public class DemoArray1 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number of Interger elements: ");
+		int n = s.nextInt();
+		Integer a[] = new Integer[n];//Array
+		System.out.println("Enter "+n+" Integer elements: ");
+		for(int i=0; i<n; i++) {
+			a[i] = new Integer(s.nextInt());
+		}//end of loop
+		
+		System.out.println("===Display using old loop===");
+		for(int i=0; i<n; i++) {
+			System.out.print(a[i].toString()+" ");
+		}//end of loop
+		
+		System.out.println("\n===Display using Entended-for(java8)===");
+		for(Integer k:a) {
+			System.out.print(k.toString()+" ");
+		}
+		
+		System.out.println("\n==Display using SplIterator<T>(java8)===");
+		Spliterator<Integer>sp = Arrays.spliterator(a);
+		sp.forEachRemaining((z) -> {
+				System.out.print(z.toString()+" ");	
+				});
+		
+		s.close();
+
+	}
+
+}
